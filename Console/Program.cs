b4b66a72144592f7a7dc2db83448ca98644c0074
Melny <marek.melnycok@gmail.com>
2025-12-25 23:38:46 +0100

    #

--- Console/Program.cs
+++ Console/Program.cs
@@ -5,3 +5,12 @@ for (int i = 0; i < 7; i++)
 {
     Console.WriteLine($"Iteration {i + 1}");
 }
+
+if (DateTime.Now.Hour < 12)
+{
+    Console.WriteLine("Good morning!");
+}
+else
+{
+    Console.WriteLine("Good afternoon!");
+}
