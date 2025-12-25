797081b3f4395e29647e8914264ad0ca5170d2b8
Melny <marek.melnycok@gmail.com>
2025-12-25 23:32:57 +0100

    #

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-for (int i = 0; i < 5; i++)
+for (int i = 0; i < 7; i++)
 {
     Console.WriteLine($"Iteration {i + 1}");
 }
